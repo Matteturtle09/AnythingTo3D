8d785c0b2238cffd623a3300d2c6607d6a62a013
Matteturtle09 <mauri.matteo16@gmail.com>
2023-12-27 11:41:55 +0000

    basic navbar

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Lato } from 'next/font/google'
+import Navbar from '@/components/ui/navbar'
 import './globals.css'
 
 const font = Lato({subsets: ['latin'], weight: "700"})
@@ -9,6 +10,13 @@ export const metadata: Metadata = {
   description: 'Generate 3D shapes using AI',
 }
 
+let menus = [
+  { title: "Home", path: "/your-path" },
+  { title: "Blog", path: "/your-path" },
+  { title: "About Us", path: "/your-path" },
+  { title: "Contact Us", path: "/your-path" },
+]
+
 export default function RootLayout({
   children,
 }: {
@@ -16,7 +24,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="it">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Navbar menus={menus} isLoggedIn={true} title='AnythingTo3D' avatarUrl='https://api.dicebear.com/7.x/notionists-neutral/svg?seed=test'></Navbar>
+        {children}
+      </body>
     </html>
   )
 }
--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,9 +1,8 @@
-import { Button } from "@/components/ui/button"
-import { MousePointer } from "lucide-react"
 
 export default function Home() {
   return (
-
-    <Button className="rounded-lg"><MousePointer/><p className="m-3">Click Me</p></Button>
+    <>
+    
+    </>
   )
 }
